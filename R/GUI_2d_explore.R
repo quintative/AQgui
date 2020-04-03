3e836b2d1137f383efb05e0afbe2d99876df4b70
Andy Quindeau <andyquindeau@Andys-Mac-mini.local>
2020-04-02 21:51:20 -0700

    incremental update

---
+++ R/GUI_2d_explore.R
@@ -0,0 +1,72 @@
+library(data.table)
+library(ggplot2)
+library(plotly)
+library(dplyr)
+library(MASS)
+# Shiny app
+library(shiny)
+library(shinydashboard)
+# Density Based Clustering
+library(fpc)
+# Developer tools
+library(devtools)
+
+#' A GUI for exploring 2d data
+#'
+#' Explore data stored in data.tables!
+#' @param data Use awesome data.table to get going...
+#' @import data.table
+#' @export
+
+GUI_2d_explore <- function(data){
+  AQ_init()
+  # Define UI
+
+  ui <- fluidPage(
+    tabsetPanel(
+      # ============================================================
+      tabPanel("data",
+               FileUI("datafile", "User Data")
+      ),
+      # ============================================================
+      tabPanel("OLS fit",
+               RegressionLinUI("linreg.pl", "simple regression")
+      ),
+      # ============================================================
+      tabPanel("k means",
+               ClusterKMUI("kmeans.pl", "km clustering")
+      ),
+      # ============================================================
+      tabPanel("hierarchical cl",
+               ClusterHCUI("hier.pl", "hierar clustering")
+      ),
+      # ============================================================
+      tabPanel("density-based cl",
+               ClusterDBUI("dens.pl", "densbase clustering")
+      )
+    )
+  )
+
+  # --------------------------------------------------------------------------------------------------------------
+  # Define server logic
+
+  server <- function(input, output, session) {
+
+    dt <- callModule(File, "datafile", reactive(data))
+
+    callModule(RegressionLin, "linreg.pl", reactive(dt()))
+
+    callModule(ClusterKM, "kmeans.pl", reactive(dt()))
+
+    callModule(ClusterHC, "hier.pl", reactive(dt()))
+
+    callModule(ClusterDB, "dens.pl", reactive(dt()))
+
+  }
+
+  # Run the application
+  # shinyApp(ui = ui, server = server)
+  runApp(list(ui = ui, server = server), launch.browser = T)
+  }
+
+
---
+++ R/module_Cluster_kmeans.R
@@ -0,0 +1,101 @@
+# Creating the "elbow" plot
+ElbowPlot <- function(data_int, k_max = 10){
+  ellbow <- data.table(k = 1:k_max, within_ss = sapply(1:k_max, function(x){kmeans(data_int[, 1:2], x)$tot.withinss}))
+  plt <- ellbow %>% 
+    plot_ly(x = ~k, y = ~within_ss) %>% 
+    add_markers(color = I("black")) %>% 
+    add_lines(color = I("black")) %>%
+    layout(showlegend = F)
+  return(plt)
+}
+
+# Run K-means and plot it
+KmeansPlot <- function(data, k){
+  
+  colnames(data) <- c("x", "y")
+  model <- kmeans(data[, 1:2], k)
+  data[, cluster := as.factor(model$cluster)]
+  centers <- as.data.table(model$centers)
+  colnames(centers) <- c("x", "y")
+  centers[, cluster := as.factor(1:.N)]
+  
+  if(nrow(data) > 5000){
+    dt.int <- as.data.table(sample_n(data, 5000))
+  } else{
+    dt.int <- data
+  }
+  
+  plt <- dt.int %>% 
+    plot_ly(x = ~x, y = ~y, color = ~cluster) %>%
+    add_markers(alpha = 0.4) %>%
+    add_annotations(data = centers,
+                    x = ~x,
+                    y = ~y,
+                    text = paste0("x: ", centers[cluster == cluster, signif(x, 3)], "\ny: ", centers[cluster == cluster, signif(y, 3)]))
+  return(plt)
+}
+
+ClusterKMUI <- function(id, label = "Clustering") {
+  
+  # Create a namespace function using the provided id
+  ns <- NS(id)
+  
+  dashboardPage(
+    
+    dashboardHeader(),
+    
+    dashboardSidebar(
+      sliderInput(ns("sl.num.max.elb"), "Max number for Elbow Plot",
+                  min = 2, max = 20, value = 10, step = 1),
+      actionButton(ns("do.elb.km"), "K-means <Elbow>"),
+      sliderInput(ns("sl.num.clust"), "Number of clusters (where applicable)",
+                  min = 2, max = 10, value = 2, step = 1),
+      actionButton(ns("do.km"), "K-means"),
+      tags$hr()),
+    
+    dashboardBody(
+        plotlyOutput(ns("plt.clust.km.elb"), width = "800px", height = "600px") ,
+        plotlyOutput(ns("plt.clust.km"), width = "800px", height = "600px")
+        )
+  )
+}
+
+# --------------------------------------------------------------------------------------------------------------
+ClusterKM <- function(input, output, session, data){
+  
+  # ============================================================
+  
+  kmeans.ellb <- eventReactive(input$do.elb.km, {
+    dt.int <- data()
+    # 1 Standardization
+    dt.int <- dt.int[(!is.na(x) & !is.na(y))]
+    dt.int[, ":=" (x_std = (x - mean(x)) / sd(x),
+                   y_std = (y - mean(y)) / sd(y))]
+    
+    plt <- ElbowPlot(dt.int, input$sl.num.max.elb)
+  
+    return(plt)
+  })
+  
+  kmeans <- eventReactive(input$do.km, {
+    dt.int <- data()
+    # 1 Standardization
+    dt.int <- dt.int[(!is.na(x) & !is.na(y))]
+    dt.int[, ":=" (x_std = (x - mean(x)) / sd(x),
+                   y_std = (y - mean(y)) / sd(y))]
+    
+    plt <- KmeansPlot(dt.int[, .(x_std, y_std)], input$sl.num.clust)
+    
+    return(plt)
+  })
+  
+  observe({
+    output$plt.clust.km.elb <- renderPlotly({kmeans.ellb()})
+  })
+  
+  observe({
+    output$plt.clust.km <- renderPlotly({kmeans()})
+  })
+}
+
+
